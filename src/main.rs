7620bc5e06da9654be8850664fc111c056bc3ac3
ququzone <xueping.yang@gmail.com>
2023-08-22 15:15:34 +0800

    add rpc error

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+use axum::{response::Html, routing::get, Router};
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(handler));
+
+    axum::Server::bind(&"127.0.0.1:3000".parse().unwrap())
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+async fn handler() -> Html<&'static str> {
+    Html("Hello world")
+}
